67dc721bd8c5222daf87e730aa2a29c0f16665e4
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-12-16 03:52:49 +0300

    basic real estate up and running

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,15 @@
 class Property < ActiveRecord::Base
-  attr_accessible :internet_included, :manager_id, :number_of_bathrooms, :number_of_bedrooms, :number_of_rooms, :owner_id, :owner_name, :parking_allotment, :period_type, :price_per_period, :property_type, :purpose, :street_address
+  attr_accessible :photo, :internet_included, :manager_id, :number_of_bathrooms, :number_of_bedrooms, :number_of_rooms, :owner_id, :owner_name, :parking_allotment, :period_type, :price_per_period, :property_type, :purpose, :street_address
 
   mount_uploader :photo, PhotoUploader
+
+  ROOMS = [['1'], ['2'], ['3'], ['4'], ['5'], ['6'], ['7'], ['8'], ['9'], ['10']]
+  BEDROOMS = [['1'], ['2'], ['3'], ['4'], ['5'], ['6'], ['7'], ['8'], ['9']]
+  BATHROOMS = [['1'], ['2'], ['3'], ['4'], ['5'], ['6'], ['7'], ['8']]
+  PARKING = [['No Parking'], ['1'], ['2'], ['3'], ['3+']]
+  PROPERTY = [['Barn'], ['Bungalow'], ['Chalet'], ['Condominium'], ['Detached'], ['Dormitory'], ['Flat'], ['Log Cabin'], ['Mansion'], ['Penthouse'], ['Semi-Detached'], ['Skyscraper'], ['Terraced'], ['Villa']]
+  PERIOD = [['per Day'], ['Weekly'], ['Monthly'], ['Annualy']]
+  PURPOSE = [['Asset'], ['Community Cause'], ['Cultural Tradition'], ['Equity'], ['Heritage'], ['Investment'], ['Primary Accomodation'], ['Tax Reduction']]
+
+
 end
